b2194c197002c897eec3db7657b5e94b3657811f
William Wedin <wcwedin@gmail.com>
2013-05-23 14:46:05 +0100

    Home and Locations controllers and views.

---
+++ Source/CasualHeroes/Models/LocationsIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CasualHeroes.Models
+{
+	public class LocationsIndexViewModel
+	{
+		public List<int> Results { get; set; }
+	}
+}
