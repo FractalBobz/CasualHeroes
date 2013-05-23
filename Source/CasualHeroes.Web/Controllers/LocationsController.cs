e68b6765841c5352cb698e561fd092e3517a2555
William Wedin <wcwedin@gmail.com>
2013-05-23 17:11:59 +0100

    Copying over Locations code from old web project.

---
+++ Source/CasualHeroes.Web/Controllers/LocationsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CasualHeroes.Models;
+
+namespace CasualHeroes.Controllers
+{
+	public class LocationsController : Controller
+	{
+		//
+		// GET: /Locations/
+
+		public ActionResult Index(decimal latitude, decimal longitude)
+		{
+			var random = new Random();
+			var resultCount = random.Next(10);
+			var results = new List<Location>();
+			for (var i = 0; i < resultCount; ++i)
+			{
+				results.Add(new Location 
+				{
+					LocationID = random.Next(5000),
+					Latitude = latitude + random.Next(60) - 30,
+					Longitude = longitude + random.Next(60) - 30,
+				});
+			}
+			return View(results);
+		}
+	}
+}
