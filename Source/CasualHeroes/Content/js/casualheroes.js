8ccfcd8a0f003420b7e1fd14e3c438cd8ff6be4b
Over Noid <overnoid@gmail.com>
2013-05-23 16:35:59 +0100

    updates

--- Source/CasualHeroes/Content/js/casualheroes.js
+++ Source/CasualHeroes/Content/js/casualheroes.js
@@ -10,13 +10,26 @@ Win.hero={
 			b='I can help with:';
 		}
 		if(b=='Need'){
-			b='I need:'
+			b='I need:';
+		}
+		if(b=='Local'){
+			b='Local Heroes:';
+			c='Map goes here!';
+		}
+		if(b=='About'){
+			b='About';
+			c=hero.c_About;
 		}
 		ihtml('field','<h1>'+b+'</h1><article>'+c+'</article>');
 	}
 };
 hero.home=ihtml('field');
+hero.c_About=ihtml('c_About');
 jss.b3d.attach3d();
-jss.hash.deal.Help=hero.hash;
-jss.hash.deal.Need=hero.hash;
+xtend('jss.hash.deal',{
+	Help:hero.hash,
+	Need:hero.hash,
+	Local:hero.hash,
+	About:hero.hash
+});
 jss.hash.hashish();
