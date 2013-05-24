682bb3e8cbca63e668f1f9d9c13e8357d533429b
William Wedin <wcwedin@gmail.com>
2013-05-24 16:04:23 +0100

    Basic admin interfaces under /html/ path.

--- Source/CasualHeroes.Web/App_Start/RouteConfig.cs
+++ Source/CasualHeroes.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,13 @@ namespace CasualHeroes.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-			
+
+			routes.MapRoute(
+				name: "Html",
+				url: "html/{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, html = true }
+			);
+
 			routes.MapRoute(
 		        name: "Default",
 		        url: "{controller}/{action}/{id}",
--- Source/CasualHeroes.Web/Controllers/RequestsController.cs
+++ Source/CasualHeroes.Web/Controllers/RequestsController.cs
@@ -10,91 +10,123 @@ using CasualHeroes.Web.Models;
 
 namespace CasualHeroes.Web.Controllers
 {
-    public class RequestsController : Controller
-    {
-        private readonly CasualHeroesEntities db = new CasualHeroesEntities();
+	public class RequestsController : Controller
+	{
+		private readonly CasualHeroesEntities db = new CasualHeroesEntities();
 
-        //
-        // GET: /Requests/
+		//
+		// GET: /Requests/
 
-		public ActionResult Index(double latitude, double longitude)
+		public ActionResult Index(double? latitude = null, double? longitude = null, bool html = false)
 		{
-			var response = new Response
+			IQueryable<Request> requests;
+			if (latitude == null || longitude == null)
 			{
-				Data = ViewModels.Request.Convert(
-					db.Requests
+				requests = db.Requests
+					.Include(r => r.User)
+					.Include(r => r.RequestTags)
+					.Where(r => r.EndDate > DateTime.UtcNow)
+					.OrderByDescending(r => r.EndDate)
+					.Take(10);
+			}
+			else
+			{
+				requests = db.Requests
 					.Include(r => r.User)
 					.Include(r => r.RequestTags)
 					.Where(r => r.EndDate > DateTime.UtcNow)
 					.Where(r => r.Latitude != null && r.Longitude != null)
-					.OrderBy(r => Math.Pow((r.Latitude.Value - latitude) * (r.Latitude.Value - latitude) + (r.Longitude.Value - longitude) * (r.Longitude.Value - longitude), 0.5))
-					.Take(10)
-					),
+					.OrderBy(r => Math.Pow((r.Latitude.Value - latitude.Value) * (r.Latitude.Value - latitude.Value) + (r.Longitude.Value - longitude.Value) * (r.Longitude.Value - longitude.Value), 0.5))
+					.Take(10);
+			}
+
+			if (html)
+			{
+				return View(requests);
+			}
+
+			var response = new Response
+			{
+				Data = ViewModels.Request.Convert(requests)
 			};
 			return Json(response, JsonRequestBehavior.AllowGet);
-        }
+		}
 
-        //
-        // GET: /Requests/Details/5
+		//
+		// GET: /Requests/Details/5
 
-        public ActionResult Details(long id = 0)
-        {
-            var request = db.Requests.Find(id);
-            if (request == null)
-            {
-                return HttpNotFound();
+		public ActionResult Details(long id = 0, bool html = false)
+		{
+			var request = db.Requests.Find(id);
+			if (request == null)
+			{
+				return HttpNotFound();
+			}
+
+			if (html)
+			{
+				return View(request);
 			}
 			var response = new Response { Data = ViewModels.Request.Convert(request) };
 			return Json(response, JsonRequestBehavior.AllowGet);
-        }
+		}
 
-        //
-        // GET: /Requests/Create
+		//
+		// GET: /Requests/Create
 
-        public ActionResult Create()
-        {
-            ViewBag.UserId = new SelectList(db.Users, "UserId", "Identifier");
-            return View();
-        }
+		public ActionResult Create()
+		{
+			ViewBag.UserId = new SelectList(db.Users, "UserId", "Identifier");
+			return View();
+		}
 
-        //
-        // POST: /Requests/Create
+		//
+		// POST: /Requests/Create
 
-        [HttpPost]
-        public ActionResult Create(Request request)
+		[HttpPost]
+		public ActionResult Create(Request request, bool html = false)
 		{
 			request.CreatedOn = DateTime.UtcNow;
 			var tags = TagsSplitter.Split(db, request.Tags);
 			request.RequestTags = tags.Select(t => new RequestTag() { Request = request, Tag = t }).ToList();
-            if (ModelState.IsValid)
-            {
-                db.Requests.Add(request);
+			if (ModelState.IsValid)
+			{
+				db.Requests.Add(request);
 				db.SaveChanges();
+
+				if (html)
+				{
+					return RedirectToAction("Details", new { id = request.RequestId });
+				}
 				return Json(new Response { Data = new { request.RequestId } });
 			}
 
+			if (html)
+			{
+				return View(request);
+			}
 			return Json(new Response { Data = "Failed" });
-        }
+		}
 
-        //
-        // GET: /Requests/Edit/5
+		//
+		// GET: /Requests/Edit/5
 
-        public ActionResult Edit(long id = 0)
-        {
-            var request = db.Requests.Find(id);
-            if (request == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.UserId = new SelectList(db.Users, "UserId", "Identifier", request.UserId);
-            return View(request);
-        }
+		public ActionResult Edit(long id = 0)
+		{
+			var request = db.Requests.Find(id);
+			if (request == null)
+			{
+				return HttpNotFound();
+			}
+			ViewBag.UserId = new SelectList(db.Users, "UserId", "Identifier", request.UserId);
+			return View(request);
+		}
 
-        //
-        // POST: /Requests/Edit/5
+		//
+		// POST: /Requests/Edit/5
 
-        [HttpPost]
-		public ActionResult Edit(long id, Request request)
+		[HttpPost]
+		public ActionResult Edit(long id, Request request, bool html = false)
 		{
 			request.RequestId = id;
 			var tags = TagsSplitter.Split(db, request.Tags);
@@ -113,29 +145,39 @@ namespace CasualHeroes.Web.Controllers
 			{
 				db.Entry(request).State = EntityState.Modified;
 				db.SaveChanges();
+
+				if (html)
+				{
+					return RedirectToAction("Details", new { id = request.RequestId });
+				}
 				return Json(new Response { Data = "Saved" });
 			}
+
+			if (html)
+			{
+				return View(request);
+			}
 			return Json(new Response { Data = "Failed" });
-        }
+		}
 
-        //
-        // GET: /Requests/Delete/5
+		//
+		// GET: /Requests/Delete/5
 
-        public ActionResult Delete(long id = 0)
-        {
-            Request request = db.Requests.Find(id);
-            if (request == null)
-            {
-                return HttpNotFound();
-            }
-            return View(request);
-        }
+		public ActionResult Delete(long id = 0)
+		{
+			Request request = db.Requests.Find(id);
+			if (request == null)
+			{
+				return HttpNotFound();
+			}
+			return View(request);
+		}
 
-        //
-        // POST: /Requests/Delete/5
+		//
+		// POST: /Requests/Delete/5
 
-        [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(long id)
+		[HttpPost, ActionName("Delete")]
+		public ActionResult DeleteConfirmed(long id, bool html = false)
 		{
 			var request = db.Requests.Find(id);
 			if (request == null)
@@ -144,13 +186,18 @@ namespace CasualHeroes.Web.Controllers
 			}
 			db.Requests.Remove(request);
 			db.SaveChanges();
-			return Json(new Response { Data = "Deleted" });
-        }
 
-        protected override void Dispose(bool disposing)
-        {
-            db.Dispose();
-            base.Dispose(disposing);
-        }
-    }
+			if (html)
+			{
+				return RedirectToAction("Index");
+			}
+			return Json(new Response { Data = "Deleted" });
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			db.Dispose();
+			base.Dispose(disposing);
+		}
+	}
 }
